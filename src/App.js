3a1773f59d430604bd50e275457f558bf86bff54
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-11-29 10:05:58 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ClickCounter from "./components/ClickCounter.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <ClickCounter />
+    </div>
+  );
+}
+
+export default App;
